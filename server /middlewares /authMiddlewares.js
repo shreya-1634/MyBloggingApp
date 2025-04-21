4c22833b6849ca525c54f7cef5e8de3370a9f640
shreya-1634 <shreyamgm16@gmail.com>
2025-04-21 14:24:57 +0530

    Create authMiddlewares.js

---
+++ server /middlewares /authMiddlewares.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config({path:'../.env'});
+
+
+module.exports = function (req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.jwt_secret);
+    req.body.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
