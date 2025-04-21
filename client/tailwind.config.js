e36a8e3e3f292b9e6b9172282bee49d78ae3a469
shreya-1634 <shreyamgm16@gmail.com>
2025-04-21 07:16:32 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,1 +1,15 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+          //primary : '#395144'
+          primary : '#2a3247'
+      }
+    },
+  },
+  plugins: [],
+}
